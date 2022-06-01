ff99d0b4090b2d1ab482180c65792df176376153
Fil31 <lolid76@gmail.com>
2022-06-01 08:23:19 +0300

    first commit

---
+++ ForEachArray/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ForEachinArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] fruits = { "apples", "bananas", "oranges", "pears", "plums", "pine-apple" };
+
+            foreach (string item in fruits) 
+            {
+                Console.WriteLine(item); 
+                if (item == "oranges")
+                {
+                    Console.WriteLine($"Boo, I don't like {item}");
+                }
+            }
+        }
+    }
+}
---
+++ FortnuteCookieHomeWork/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FortuneCookie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fortune Cookie. What awaits you today?");
+            string[] fortune = new string[]
+            {
+                "Win a Jackpot",
+                "Fall in love",
+                "Get a love letter",
+                "See a unicorn in the street",
+                "Miss the bus",
+            };
+            Console.WriteLine("Your cookie is: {0}", fortune.GetValue(new Random().Next(fortune.Length)));
+        }
+    }
+}
